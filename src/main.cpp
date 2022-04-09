e6150fd7bb519c5c9b510711a08e93574a9d1a3a
Ángel Talero <angelgotalero@outlook.com>
2022-04-09 13:07:32 -0500

    Renamed some files so their are more accurate

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <cmake_config.h> /* cmake config variables */
+
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char *args[])
+{
+    std::cout << "Hello World!" << std::endl;
+    return EXIT_SUCCESS;
+}
